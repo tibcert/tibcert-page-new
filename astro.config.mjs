e7f70fa2a105db3e53554312bcf6eb01790f259e
tibcert <info@tibcert.org>
2026-03-02 13:13:45 +0530

    added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ["blog.tibcert.org"],
+  },
   vite: {
     plugins: [tailwindcss()]
   }
