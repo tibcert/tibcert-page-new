4186d4eb023ba7f855b112e64781c1d4f915859a
tibcert <info@tibcert.org>
2026-07-22 12:45:56 +0530

    added robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tibcert.org',
+  integrations: [sitemap()],
   image: {
     domains: ["blog.tibcert.org"],
     service: passthroughImageService(),
